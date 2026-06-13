30ad4503e3c1405f4e7c887e5300276c91efe811
nepalhimaladventures431 <nepalhimaladventures431@gmail.com>
2026-06-13 18:28:44 +0545

    initial commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React, {useState} from 'react'
+import './header.css'
+
+export default function Header() {
+  return (
+    <header id="header" className='header d-flex align-items-center fixed-top'>
+        <div className='container-fluid container-xl d-flex align-items-center justify-content-between'>
+            <a href='/' className='logo d-flex align-items-center'>
+            <h1>Chamkilo</h1>
+            </a>
+        </div>
+
+    </header>
+  )
+}
